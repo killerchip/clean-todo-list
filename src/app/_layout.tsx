f0dffcf404bb6d1fa2822efcb79616ff3ce68f67
Costas Ioannou <ioannou.dev@gmail.com>
2024-04-14 13:21:10 +0300

    add mobx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { Slot } from 'expo-router';
+import '../mobx';
 
+// eslint-disable-next-line mobx/missing-observer
 export default function HomeLayout() {
   return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native';
 
+// eslint-disable-next-line mobx/missing-observer
 export default function HomePage() {
   return (
     <View>
