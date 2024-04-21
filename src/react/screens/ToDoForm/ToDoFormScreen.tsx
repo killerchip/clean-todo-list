c10b2b9d4782689ad2e0f2335bf54fd57de7d270
Costas Ioannou <ioannou.dev@gmail.com>
2024-04-21 11:45:15 +0300

    a bit of style in form

--- src/react/screens/ToDoForm/ToDoFormScreen.tsx
+++ src/react/screens/ToDoForm/ToDoFormScreen.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from 'expo-checkbox';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { useToDoListScreenPresenter } from './ToDoFormScreenPresenter';
 
@@ -35,28 +35,84 @@ export const ToDoFormScreen = observer(function ToDoFormScreen() {
   };
 
   return (
-    <View>
+    <>
       <Stack.Screen options={{ title: presenter.pageTitle }} />
-      <Text>Id: {presenter.formData.id ?? '-'}</Text>
-      <Text>Task Title</Text>
-      <TextInput
-        value={presenter.formData.title}
-        onChangeText={onChangeTitle}
-      />
-      <Text>Task Description</Text>
-      <TextInput
-        value={presenter.formData.description ?? ''}
-        onChangeText={onChangeDescription}
-      />
-      <Text>Is Done</Text>
-      <Checkbox
-        value={presenter.formData.isDone}
-        onValueChange={onChangeIsDone}
-      />
-      <Button onPress={onSubmit} title='Save' />
-      <Button onPress={onCancel} title='Cancel' />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+          <View style={styles.idContainer}>
+            <Text style={styles.idLabel}>
+              Id: {presenter.formData.id ?? '-'}
+            </Text>
+            <View style={styles.doneContainer}>
+              <Text>Is Done</Text>
+              <Checkbox
+                value={presenter.formData.isDone}
+                onValueChange={onChangeIsDone}
+                style={styles.checkbox}
+              />
+            </View>
+          </View>
+          <Text style={styles.label}>Task Title</Text>
+          <TextInput
+            style={styles.textInput}
+            value={presenter.formData.title}
+            onChangeText={onChangeTitle}
+          />
+          <Text style={styles.label}>Task Description</Text>
+          <TextInput
+            style={[styles.textInput, styles.textArea]}
+            value={presenter.formData.description ?? ''}
+            onChangeText={onChangeDescription}
+            multiline
+          />
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button onPress={onCancel} title='Cancel' color='gray' />
+          <Button onPress={onSubmit} title='Save' />
+        </View>
+      </View>
+    </>
   );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  formContainer: {},
+  idContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  doneContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  checkbox: { marginLeft: 6 },
+  idLabel: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    color: 'gray',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  textInput: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    padding: 5,
+  },
+  textArea: {
+    minHeight: 100,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 16,
+  },
 });
 
 // ToDo: Properly style the screen
